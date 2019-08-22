0bd34d192f888f1ab735aae94d61799783b5be12
0730chen <1252331215@qq.com>
2019-08-22 18:32:48 +0800

    博客首页

--- src/components/blog.vue
+++ src/components/blog.vue
@@ -1,49 +1,117 @@
 <template>
     <div class="context">
+        <!-- <canvas class="canvas"></canvas> -->
+        <div class="button" @click="change">
+        </div>
         <el-container>
             <el-header>
                 <el-button type="primary" icon="el-icon-edit" @click="manger">管理</el-button>
-                <el-dropdown>
-                <span class="el-dropdown-link">
-                 标签<i class="el-icon-arrow-down el-icon--right"></i>
-                 </span>
-                <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item>JS</el-dropdown-item>
-                <el-dropdown-item>Css</el-dropdown-item>
-                <el-dropdown-item>后端</el-dropdown-item>
-                <el-dropdown-item>HTTP</el-dropdown-item>
-                <el-dropdown-item>随手记</el-dropdown-item>
-               
-                 </el-dropdown-menu>
-                </el-dropdown>
             </el-header>
-            <el-container>
-                <el-main>
-                    <el-menu>
-                        <el-menu-item idnex="1">首页
-                        </el-menu-item>
-                        <el-submenu index="2">
-                            <template slot="title">笔记</template>
-                        </el-submenu>
-                        <el-menu-item index="3" >每日学习    </el-menu-item>
-                        <el-menu-item index="4">算法</el-menu-item>
-                    </el-menu>
-                </el-main>
-                <el-aside>
-                    <div class="article" v-for="(item,index) of article" :key="index">
-                    {{item.title}}
-                    <el-divider></el-divider>
-                </div> 
-                </el-aside>
-            </el-container>
-            
+            <transition name="show">
+            <div  class="cover" v-show="flag"><menuList></menuList></div>
+            </transition>
         </el-container>
+        <transition name="show">
+        <div class="mycover" v-show="flag" @click="change"></div></transition>
+        <div class="footer">
+            <div v-for="(item,index) of footer" :key="index" class="item">
+                <img :src="item.img" alt="">
+               <div class="item-path"><router-link :to="{path:item.path}">{{item.name}}</router-link></div>
+                </div>	
+        </div>
     </div>
 </template>
 <style scoped>
+canvas{
+ position: absolute;
+}
+.item-path{
+    position: absolute;
+    top:0.5rem;
+    left: 0.3rem;
+}
+img{
+    position: absolute;
+    height: 0.5rem;
+    width: 0.5rem;
+    top: 0;
+    left:0.25rem;
+}
+a{
+    text-decoration:none;
+    color: black;
+}
+
+.item{
+    position: relative;
+    width: 15%;
+    height: 100%;
+    flex: 1;
+}
+.footer{
+    position: fixed;
+    bottom: 0;
+    height: 1.5rem;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items:center;
+}
+/* .show-enter, .show-leaver-to{
+    transition: all 1s ease;
+    transform: translateX(100px)
+}
+.show-enter-active, .show-leaver-active{
+    transition: opacity 5s ease-out;
+    transition: all 0.5s ease;
+} */
+.show-enter-active{
+    transition: all 0.5s ease;
+}
+.show-leave-active{
+   transition: all 0.5s ease;
+}
+.show-enter{
+    transform: translateX(100px);
+    opacity: 0;
+}
+.show-leave-to{
+    transform: translateX(400px);
+}
+.button{
+    background:url('../assets/menu.png') no-repeat 100% 100%;
+    background-size: 75%;
+    position: absolute;
+    height: 7%;
+    width: 10%;
+    top: 0;
+    right: 0.12rem;
+    z-index: 100;
+}
+.mycover{
+    width: 100%;
+    height: 100%;
+    position:fixed;
+    background-color: rgba(0,0,0,.7);
+    top: 0;
+    left: 0;
+    z-index: 99;
+    opacity: 0.5;
+}
+.cover{
+    width: 100%;
+    height: 100%;
+    position: absolute;
+}
+.el-header{
+    position: relative;
+}
 .el-button--primary{
+    position: absolute;
     background-color: #4f5458;
-    transform: translateY(-0.25rem)
+    left:0;
+    top:0.1rem;
 }
 .el-menu{
     width: 100%;
@@ -110,36 +178,97 @@
 
 </style>
 <script>
+import menu from '../components/menuList'
 import axios from 'axios'
 import Element from 'element-ui'
+const book = require('../assets/book.png')
+const memory = require('../assets/pen.png')
+const penceil = require('../assets/penceil.png')
+const star = require('../assets/star.png')
+// console.log(book)
+// console.log(memory)
+// console.log(penceil)
+// console.log(star)
 // console.log(Element)
 export default {
     data(){
         return {
-            article:[
+            article:[],
+            flag:false,
+            footer:[
                 {
-                    title:'JavaScript',
-                    context:'ssss',
+                    name:'学习',
+                    path:'/learn',
+                    img:book,
                 },
                 {
-                    title:'这是文章',
-                    context:'内容'
+                    name:'记忆',
+                    path:'/memory',
+                    img:memory,
+                },
+                {
+                    name:'笔记',
+                    path:'/note',
+                    img:penceil,
+                },
+                {
+                    name:'倒计时',
+                    path:'/memory',
+                    img:star,
                 }
-
             ]
+            
         }
     },
-    component:{
-    
+    components:{
+        'menuList':menu
     },
     methods:{
         manger(){
             this.$router.push({'path':'/loging'})
         },
         getSon(){
-            axios.get('http://chentian.ltd/')
-        }
+            axios.get('/api/addArticle').then(res => {
+                console.log(res.data)
+                this.article = res.data
+            })
+        },
+        change(){
+            // console.log('点击')
+            this.flag =! this.flag
+            console.log(this.flag)
+        },
+        // mystart(){
+        //     var canvas = document.getElementById('canvas')
+        //     ctx = canvas.getContext('2d'),
+        //     w = canvas.width = window.innerWidth
+        //     h = canvas.height = window.innerHeight
+        //     let hue = 217
+        //     let stars = []
+        //     let count = 0 
+        //     maxStart = 1200
+        //     var canvas2 = document.createElement('canvas')
+        //     ctx2 = canvas2.getContext('2d')
+        //     canvas2.width=100
+        //     canvas2.height=100
+        //     var half = canvas2.width/2
+        //     gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+        //     gradient2.addColorStop(0.025, '#fff');
+        //     gradient2.addColorStop(0.1, 'hsl(' + hue + ', 61%, 33%)');
+        //     gradient2.addColorStop(0.25, 'hsl(' + hue + ', 64%, 6%)');
+        //     gradient2.addColorStop(1, 'transparent');
 
+        //     ctx2.fillStyle = gradient2;
+        //     ctx2.beginPath();
+        //     ctx2.arc(half, half, half, 0, Math.PI * 2);
+        //     ctx2.fill();
+
+        // },
+        
+
+    },
+    created(){
+        
     }
 
 }
---
+++ src/components/menuList.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="mean">
+        <div class="avatar">
+            <img src="../assets/logo.png" alt="">
+        </div>
+        <div v-for="(menu,index) of menuLs" :key="index" class="lists">
+            <div class="list">{{menu.name}}</div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.lists{
+    position: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+.list{
+    height: 1rem;
+}
+.avatar{
+    position: relative;
+
+}
+.mean{
+    position: absolute;
+    z-index: 120;
+    height: 100%;
+    width: 70%;
+    right: 0;
+    border:1px solid black;
+    color: aliceblue;
+    background-color: black;
+}
+    
+</style>
+<script>
+export default {
+    data(){
+        return {
+            menuLs:[
+                {
+                    name:'首页',
+                    path:'',
+                },
+                {
+                    name:'假期倒计时',
+                    path:'',
+
+                },
+                {
+                    name:'笔记',
+                    path:'',
+                }
+            ]
+        }
+    }
+}
+</script>
